209f33cd24d5abfab4d1e8de77b6bf86da076b2b
mpxfactor <tancmpsinc@gmail.com>
2023-12-30 15:41:28 +0530

    added tailwind css

--- src/main/java/com/thymeleaf/proj/RootController.java
+++ src/main/java/com/thymeleaf/proj/RootController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.List;
 
 @Controller
@@ -13,8 +16,18 @@ public class RootController {
 
     @GetMapping
     public String index(Model model) {
+        List<Student> studentsList = List.of(new Student(1, "Punith"), new Student(2, "Lokesh"),
+                new Student(3, "John"));
+
         model.addAttribute("title", "Thymeleaf title from controller");
-        model.addAttribute("scientists", List.of("Albert Einstein", "Newton", "Hawking", "Punith"));
+        model.addAttribute("students", studentsList);
         return "index";
     }
+}
+
+@Data
+@AllArgsConstructor
+class Student {
+    private int id;
+    private String name;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/main/resources/templates/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", "sans"],
+      },
+    },
+  },
+  plugins: [],
+};
